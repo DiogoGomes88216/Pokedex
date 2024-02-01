4e625b0848c1597b8a2614710f1fe0d274a9cd55
DiogoGomes88216 <t3diogogomes@hotmail.com>
2024-02-01 15:26:50 +0000

    Updated Dependencies

--- app/src/main/java/com/example/pokedex/presentation/pokemonInfo/PokemonInfoScreen.kt
+++ app/src/main/java/com/example/pokedex/presentation/pokemonInfo/PokemonInfoScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
